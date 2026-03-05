555caf5fb0a04e3aa2037ad0b9f2d484e97ce106
Nady Emad Nady <nady240102590@sut.edu.eg>
2026-03-05 10:28:15 +0200

    01
    01

--- resources/views/b.blade.php
+++ resources/views/b.blade.php
@@ -1,7 +1,39 @@
-@if (count($records) === 1)
-    I have one record!
-@elseif (count($records) > 1)
-    I have multiple records!
-@else
-    I don't have any records!
-@endif
+@extends('layouts.app')
+
+@section('title', 'Prime Numbers')
+
+@section('content')
+    <div class="card m-4">
+        <div class="card-header">
+            @if (count($records) === 1)
+                I have one record!
+            @elseif (count($records) > 1)
+                I have multiple records!
+            @else
+                I don't have any records!
+            @endif
+        </div>
+
+        <div class="card-body">
+            @php
+                function isPrime($num) {
+                    if ($num < 2) return false;
+                    if ($num == 2) return true;
+                    if ($num % 2 == 0) return false;
+                    for ($i = 3; $i <= sqrt($num); $i += 2) {
+                        if ($num % $i == 0) return false;
+                    }
+                    return true;
+                }
+            @endphp
+            
+            @foreach (range(1, 100) as $i)
+                @if(isPrime($i))
+                    <span class="badge bg-primary">{{$i}}</span>
+                @else
+                    <span class="badge bg-secondary">{{$i}}</span>
+                @endif
+            @endforeach
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,24 @@ Route::get('/square-numbers', function () {
 });
 
 Route::get('/b', function () {
-    return view('b');
+    // تعريف متغير records للاختبار
+    $records = [1, 2, 3]; // يمكن تغييره حسب الحاجة
+    
+    // دالة للتحقق من الأعداد الأولية
+    function isPrime($num) {
+        if ($num < 2) return false;
+        if ($num == 2) return true;
+        if ($num % 2 == 0) return false;
+        for ($i = 3; $i <= sqrt($num); $i += 2) {
+            if ($num % $i == 0) return false;
+        }
+        return true;
+    }
+    
+    return view('b', compact('records'));
+});
+
+Route::get('/multable/{number?}', function ($number = null) {
+    $j = $number??2;
+    return view('multable', compact('j')); //multable.blade.php
 });
